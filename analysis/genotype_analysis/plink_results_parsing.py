ff68bf655cddeda6a6fe04ffdf5fe9027619a847
Rachit Kumar <rkumar.biz@gmail.com>
2023-01-14 12:01:40 -0500

    Add finished script for PRS calculations

--- analysis/genotype_analysis/plink_results_parsing.py
+++ analysis/genotype_analysis/plink_results_parsing.py
@@ -2,8 +2,9 @@ import argparse
 import pandas as pd
 import os, glob
 import subprocess
-from pathlib import Path
 import numpy as np
+
+import matplotlib.pyplot as plt
 
 import ipdb
 
@@ -18,13 +19,34 @@ def main(args):
 
     filt_results = filt_results[['ID', 'BETA', 'P']]
 
+    # Output some files with data
     filt_results.to_csv('{}_filtered_results.csv'.format(args.output_prefix), sep='\t', index=False)
-
     filt_results['ID'].to_csv('{}_filtered_snplist.txt'.format(args.output_prefix), index=False, header=None)
+
+
+    # Use the filtered file to run Plink and get the matrix of genotypes to variants (smaller)
+    plink_cmd = ['plink', 
+        '--bfile', args.input_bfile_prefix, 
+        '--recode', 'A', 
+        '--out', args.output_prefix, 
+        '--extract', '{}_filtered_snplist.txt'.format(args.output_prefix)]
+
+    subprocess.run(plink_cmd)
+    #plink --bfile 5_Genotype_Data_All_Years --recode A --out maize_results_test --extract maize_trait_selected_filtered_snplist.txt
+
+    # '{}.raw'.format(args.output_prefix) is where the matrix of samples to genotypes will be
+
+    # All credits to Rasika for writing the below
+    sample_geno_matrix = pd.read_csv('{}.raw'.format(args.output_prefix), sep='\t')
+    geno_effect_matrix = filt_results[['ID, BETA']].set_index('ID')
+
+    sample_geno_matrix = sample_geno_matrix.drop('PAT', 'MAT', 'SEX', 'PHENOTYPE', 'FID').fillna(0)
+
+    sample_prs = np.matmul(sample_geno_matrix, geno_effect_matrix)
+    sample_prs.to_csv('{}_prs_scores.txt'.format(args.output_prefix), header=None)
 
     print("Completed!")
 
-    ipdb.set_trace()
 
 
 
@@ -35,6 +57,9 @@ if __name__ == '__main__':
 
     parser.add_argument("--input-file", type=str, default=None,
             help="input file with the Plink summary statistics")
+
+    parser.add_argument("--input-bfile-prefix", type=str, default=None,
+            help="input file with the actual raw Plink data")
 
     parser.add_argument("--p-cutoff", type=float, default=1e-100,
             help="P-value cutoff for variant selection")
@@ -47,6 +72,9 @@ if __name__ == '__main__':
 
 
     args = parser.parse_args()
+
+    # Example run:
+    # python plink_results_parsing.py --input-file maize_results_test.Yield_Mg_ha.glm.linear --input-bfile-prefix 5_Genotype_Data_All_Years --p-cutoff 1e-100 --beta-cutoff 0 --output-prefix maize_trait_selected
 
     
     print(args)
